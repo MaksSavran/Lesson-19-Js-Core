81d44b41bc2a5716ec929241b4d5a231e7569158
MaksSavran <maks.v.savran@gmail.com>
2021-07-15 18:50:08 +0300

    Lesson19 Basic RegExp

---
+++ script/script.js
@@ -0,0 +1,70 @@
+
+const getID = id => document.getElementById(id);
+
+let inputArray = [
+    {
+        inputId: 'firstName',
+        regExp: /^[a-zA-Z]{1,20}$/,
+        isValid: false
+    },
+    {
+        inputId: 'lastName',
+        regExp: /^[a-zA-Z]{1,20}$/,
+        isValid: false
+    },
+    {
+        inputId: 'email',
+        regExp: /^[\w\.\-]+@[\w\.]+$/,
+        isValid: false
+    },
+    {
+        inputId: 'password',
+        regExp: /^\w{8,15}$/,
+        isValid: false
+    }];
+
+
+setValidation(inputArray);
+
+getID('agreement').addEventListener('change', () => {
+    checkValidation(inputArray);
+});
+
+getID('signUp').addEventListener('click', () => {
+    document.querySelector('.modalContainer').classList.remove('hidden');
+});
+
+getID('startExploring').addEventListener('click', () => clearForm(inputArray));
+
+
+function setValidation(inputInformation) {
+    inputInformation.forEach(elem => {
+        getID(elem.inputId).addEventListener('input', () => {
+
+            if (elem.regExp.test(getID(elem.inputId).value)) {
+                getID(elem.inputId).classList.add('is-valid');
+                getID(elem.inputId).classList.remove('is-invalid');
+                elem.isValid = true;
+            } else {
+                getID(elem.inputId).classList.add('is-invalid');
+                getID(elem.inputId).classList.remove('is-valid');
+                elem.isValid = false;
+            }
+            checkValidation(inputArray);
+        })
+    });
+}
+
+function checkValidation(validInformation) {
+    let isValid = validInformation.every(({ isValid }) => isValid) && getID('agreement').checked;
+
+    getID('signUp').disabled = !isValid;
+};
+function clearForm(inputArray) {
+    document.querySelector('.modalContainer').classList.add('hidden');
+    getID('form').reset();
+    inputArray.forEach(elem => {
+        elem.isValid = false;
+        getID(elem.inputId).classList.remove('is-valid');
+    });
+}
